d442150622b6f607ccd5f9bc3fbe5403f22f7dbc
Dean de Bree <ddebree@gmail.com>
2020-11-22 15:11:06 +0200

    Use alpha beta pruning

--- src/main/java/io/github/ddebree/connect4/Solver.java
+++ src/main/java/io/github/ddebree/connect4/Solver.java
@@ -16,19 +16,22 @@ public class Solver {
 
     public int solve(final Position postion) {
         nodeCount = 0;
-        return negamax(postion);
+        return negamax(postion,
+                -Position.WIDTH * Position.HEIGHT / 2,
+                Position.WIDTH * Position.HEIGHT / 2);
     }
 
-    /*
-     * Recursively solve a connect 4 position using negamax variant of min-max algorithm.
-     * @return the score of a position:
-     *  - 0 for a draw game
-     *  - positive score if you can win whatever your opponent is playing. Your score is
-     *    the number of moves before the end you can win (the faster you win, the higher your score)
-     *  - negative score if your opponent can force you to lose. Your score is the opposite of
-     *    the number of moves before the end you will lose (the faster you lose, the lower your score).
+    /**
+     * Reccursively score connect 4 position using negamax variant of alpha-beta algorithm.
+     * @param: alpha < beta, a score window within which we are evaluating the position.
+     *
+     * @return the exact score, an upper or lower bound score depending of the case:
+     * - if true score of position <= alpha then true score <= return value <= alpha
+     * - if true score of position >= beta then beta <= return value <= true score
+     * - if alpha <= true score <= beta then return value = true score
      */
-    private int negamax(final Position position) {
+    private int negamax(final Position position, int alpha, int beta) {
+        assert(alpha < beta);
         nodeCount++; // increment counter of explored nodes
 
         if (position.nbMoves() == Position.WIDTH * Position.HEIGHT) {
@@ -43,21 +46,28 @@ public class Solver {
             }
         }
 
-        int bestScore = -Position.WIDTH * Position.HEIGHT; // init the best possible score with a lower bound of score.
+        int max = (Position.WIDTH * Position.HEIGHT-1 - position.nbMoves()) / 2;	// upper bound of our score as we cannot win immediately
+        if (beta > max) {
+            beta = max;                     // there is no need to keep beta above our max possible score.
+            if (alpha >= beta) return beta;  // prune the exploration if the [alpha;beta] window is empty.
+        }
 
         for (int x = 0; x < Position.WIDTH; x++) {
             // compute the score of all possible next move and keep the best one
             final int col = columnOrder[x];
             if (position.canPlay(col)) {
                 final Position newPosition = position.nextPostion(col);
-                int score = -negamax(newPosition); // If current player plays col x, his score will be the opposite of opponent's score after playing col x
-                if (score > bestScore) {
-                    bestScore = score; // keep track of best possible score so far.
-                }
+                int score = -negamax(newPosition, -beta, -alpha); // explore opponent's score within [-beta;-alpha] windows:
+                // no need to have good precision for score better than beta (opponent's score worse than -beta)
+                // no need to check for score worse than alpha (opponent's score worse better than -alpha)
+
+                if (score >= beta) return score;  // prune the exploration if we find a possible move better than what we were looking for.
+                if (score > alpha) alpha = score; // reduce the [alpha;beta] window for next exploration, as we only
+                // need to search for a position that is better than the best so far.
             }
         }
 
-        return bestScore;
+        return alpha;
     }
 
     public long getNodeCount() {
